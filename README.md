d3257ec7b6003b883430d665e7aea87f8a62fe62
Hiroyuki Adachi <hiroxpepe@gmail.com>
2020-06-20 21:07:02 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # minimum-spring-mvc
 A minimum spring mvc project.
+
+## Overview
+It's a minimum project of Spring MVC by **not using** Spring Boot and Spring initializr. Though it will help you to understand the structure of Spring MVC simply.
+
+## What tools to use.
+- Maven (* also JDK)
+
+## How to build.
+```sh
+$ cd PathToThisREADME.md
+$ mvn install
+```
+
+## How to run.
+```sh
+$ cd PathToThisREADME.md
+$ mvn jetty:run
+```
+- check if it can access.  
+http://localhost:8080/
+
+## License
+It's released under version 2.0 of the [Apache License](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/main/java/org/examproject/controller/HelloController.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.examproject.controller;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.examproject.form.HelloForm;
+
+/**
+ * @author h.adachi
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class HelloController {
+
+    @NonNull
+    private final HelloForm helloForm;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // public methods
+
+    @RequestMapping(
+        value="/hello.html",
+        method=RequestMethod.GET
+    )
+    public String doGet(ModelMap model) {
+        model.addAttribute(helloForm);
+        return "form";
+    }
+
+    @RequestMapping(
+        value="/hello.html",
+        method=RequestMethod.POST
+    )
+    public String doPost(
+        @ModelAttribute HelloForm helloForm
+    ) {
+        String greeting = "Hello! your name is " + helloForm.getName() + ".";
+        log.info(greeting);
+        helloForm.setName(greeting);
+        return "show";
+    }
+
+}
